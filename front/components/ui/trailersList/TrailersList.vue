9b86432f30c6d6cb9a8a731d24dfcee5d9441774
dan-arki <100216647+dan-arki@users.noreply.github.com>
2024-05-16 13:59:12 +0200

    random trailers added

--- front/components/ui/trailersList/TrailersList.vue
+++ front/components/ui/trailersList/TrailersList.vue
@@ -1,25 +1,62 @@
 <template>
-  <div class="flex gap-5">
-    <div class="w-96 h-60 rounded-lg border">
-      <img
-        src=""
-        class="w-full h-full object-cover rounded-lg"
-        alt="trailerImage"
-      />
-    </div>
-    <div class="w-96 h-60 rounded-lg border">
-      <img
-        src=""
-        class="w-full h-full object-cover rounded-lg"
-        alt="trailerImage"
-      />
-    </div>
-    <div class="w-96 h-60 rounded-lg border">
-      <img
-        src=""
-        class="w-full h-full object-cover rounded-lg"
-        alt="trailerImage"
-      />
+  <div class="flex gap-5 flex-wrap">
+    <div
+      v-for="trailer in selectedTrailers"
+      :key="trailer.id"
+      class="w-96 h-60 rounded-lg border overflow-hidden"
+    >
+      <iframe
+        class="w-full h-full object-cover"
+        controls
+        :src="getYoutubeEmbededUrl(trailer.trailer_url)"
+      >
+        Your browser does not support the video tag.
+      </iframe>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      trailers: [],
+      selectedTrailers: [],
+    };
+  },
+
+  mounted() {
+    this.getTrailers();
+  },
+
+  methods: {
+    async getTrailers() {
+      try {
+        const response = await fetch("http://localhost:3333/media", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await response.json();
+        console.log(data);
+        this.trailers = data;
+        this.selectRandomTrailers();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    getYoutubeEmbededUrl(url) {
+      const videoId = url.split("v=")[1];
+      return `https://www.youtube.com/embed/${videoId}`;
+    },
+
+    selectRandomTrailers() {
+      // Choix aléatoire de 3 trailers
+      const shuffled = this.trailers.sort(() => 0.5 - Math.random());
+      this.selectedTrailers = shuffled.slice(0, 3);
+    },
+  },
+};
+</script>
